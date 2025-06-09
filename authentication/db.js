1191c322cb60d190fd9b8212a92d06be9dd74afe
Keshav Lohani <139124045+keshav-1303@users.noreply.github.com>
2025-06-09 23:07:37 +0530

    Delete authentication directory

--- authentication/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-function connectDB() {
-    console.log("Connecting to DB...");
-    return mongoose.connect("mongodb://db-service:27017/ecommerce");
-}
-
-module.exports = connectDB;
